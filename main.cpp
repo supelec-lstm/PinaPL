a618df8b97187f66963a17a19c517cd04bad72fe
HemeryJu <ju.hemery37@gmail.com>
2016-09-25 14:53:46 +0200

    Modification du squelette

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@
 //
 
 #include <iostream>
+#include "neurone.h"
+
+int test(int x)
+{
+    return 2*x;
+}
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	int (*func)(int);
+	func = &test;
+	std::cout << func(2);
     return 0;
 }
--- neurone.h
+++ neurone.h
@@ -3,18 +3,33 @@
 
 #include <string>
 
+enum ActiveFunc {SIG1, SIG2, HEA, ARCT};
+enum CompFunc {SUM, DIST};
+
 class Neurone
 {
     public:
 
+    Neurone(int in, int out, ActiveFunc activFunc, CompFunc compFunc);
+    double calcul(double x);
+
     private:
 
-    int nbin;
-    int nbout;
-    Neurone next[];
-    double (*comp) (double);
-    double (*activ) (double);
+    int nbin;                      //Nombre d'entrées
+    int nbout;                     //Nombre de sorties
+    double* weight;                //Tableau des poids
+    double (*comp) (double);       //Fonction de composition
+    double (*activ) (double);      //Fonction d'activation
+
+    static double actSig1(double x);
+    static double actSig2(double x);
+    static double actHea(double x);
+    static double actArct(double x);
+
+    static double compSum(double x[]);
+    static double compDist(double x[]);
 };
 
 
 #endif
+
