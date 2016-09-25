fda119842182fc34668fa1f44159fc0ac44e5e2d
Sylvain Pascou <spascou@gmail.com>
2016-09-25 13:20:27 +0200

    Projet Eclipse

---
+++ src/PinaPL.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : PinaPL.cpp
+// Author      : PinaPL Team
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
