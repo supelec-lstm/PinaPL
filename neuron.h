c1492da856e148eeead29ab66a4f87942508b1c1
Hugo Hervieux <hugo.hervieux@rez-gif.supelec.fr>
2016-09-26 11:33:45 +0200

    English translation / Naming convention

---
+++ neuron.h
@@ -0,0 +1,34 @@
+#ifndef DEF_NEURONE
+#define DEF_NEURONE
+
+#include <string>
+
+enum ActivationFunction {SIG, HEA, ARCT};
+enum CompositionFunction {SUM, DIST};
+
+class Neuron
+{
+    public:
+
+    Neuron(int in, int out, ActivationFunction activationFunction, CompositionFunction compositionFunction);
+    double compute(double x[]);
+
+    private:
+
+    int inputCount;
+    int outputCount;
+    double* weight;
+    double (*composition) (double[], int);
+    double (*activation) (double);
+
+    static double activationSignal(double x);
+    static double activationHeavyside(double x);
+    static double activationArctan(double x);
+
+    static double compositionSum(double x[], int n);
+    static double compositionDist(double x[], int n);
+};
+
+
+#endif
+
