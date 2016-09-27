d77aa88f1ca869b41afc70fe494abdb6085a2d59
Maxime Amossé <maxime@amosse.com>
2016-09-27 11:52:54 +0200

    Attempt at NeuronNetwork class (but far from finished)
    Current questions:
    - How to differentiate input and output neurons?
    - How to identify neurons in the network?
    - What data structure?

--- neuronNetwork/neuronNetwork.cpp
+++ neuronNetwork/neuronNetwork.cpp
@@ -2,3 +2,53 @@
 //  neuronNetwork.cpp
 //  PinaPL
 //
+
+#include "neuronNetwork.hpp"
+
+
+NeuronNetwork::NeuronNetwork() {
+    neurons = vector<Neuron>();
+    connections = vector<vector<unsigned long>>();
+}
+
+// Building the network
+
+unsigned long NeuronNetwork::addNeuron(Neuron neuron, NeuronType type) {
+#warning type is not implemented yet
+    neurons.push_back(neuron);
+    connections.push_back(vector<unsigned long>());
+    return neurons.size();
+}
+
+void NeuronNetwork::addConnection(unsigned long from, unsigned long to) {
+    connections.at(from).push_back(to);
+}
+
+void NeuronNetwork::disconnectNeuron(unsigned long neuronIndex) {
+    connections.at(neuronIndex) = vector<unsigned long>();
+}
+
+// Getters and Setters
+
+vector<Neuron> NeuronNetwork::getNeurons() const {
+    return neurons;
+}
+
+vector<vector<unsigned long>> NeuronNetwork::getConnections() const {
+    return connections;
+}
+
+void NeuronNetwork::setNeurons(vector<Neuron> newNeurons) {
+    neurons = vector<Neuron>(newNeurons);
+    connections = vector<vector<unsigned long>>(newNeurons.size(), vector<unsigned long>());
+}
+
+void NeuronNetwork::setConnections(vector<vector<unsigned long>> newConnections) {
+    connections = vector<vector<unsigned long>>(newConnections);
+}
+
+// Network flow
+
+void NeuronNetwork::compute() {
+#warning not implemented
+}
--- neuronNetwork/neuronNetwork.hpp
+++ neuronNetwork/neuronNetwork.hpp
@@ -2,3 +2,44 @@
 //  neuronNetwork.hpp
 //  PinaPL
 //
+
+#ifndef DEF_NEURONNETWORK
+#define DEF_NEURONNETWORK
+
+#include <vector>
+
+#include "../simpleNeuron/neuron.hpp"
+
+
+using namespace std;
+
+enum NeuronType : unsigned long {
+    NeuronTypeInternal = 0,
+    NeuronTypeInput = 1 << 0,
+    NeuronTypeOutput = 1 << 1,
+};
+
+class NeuronNetwork {
+    vector<Neuron> neurons;
+    vector<vector<unsigned long>> connections;
+    
+    vector<unsigned long> inputNeurons;
+    vector<double> inputValues;
+    
+public:
+    NeuronNetwork();
+    
+    unsigned long addNeuron(Neuron neuron, NeuronType type);
+    void changeNeuronType(unsigned long neuronIndex, NeuronType newType);
+    void addConnection(unsigned long from, unsigned long to);
+    void disconnectNeuron(unsigned long neuronIndex);
+    
+    vector<Neuron> getNeurons() const;
+    vector<vector<unsigned long>> getConnections() const;
+    void setNeurons(vector<Neuron> newNeurons);
+    void setConnections(vector<vector<unsigned long>> newConnections);
+    
+    void compute();
+};
+
+#endif // DEF_NEURONNETWORK
