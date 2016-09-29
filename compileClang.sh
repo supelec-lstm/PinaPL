784d4e23381607df9823d309b8ad61bc00642c2d
Sylvain Pascou <spascou@gmail.com>
2016-09-29 09:55:23 +0200

    adaptation du script de compilation Clang

--- compileClang.sh
+++ compileClang.sh
@@ -1,4 +1,5 @@
 clang++ -std=c++11 -c console/console.cpp -o console.o
 clang++ -std=c++11 -c simpleNeuron/neuron.cpp -o neuron.o
-clang++ -std=c++11 -o testBuild main.cpp console.o neuron.o
+clang++ -std=c++11 -c neuronNetwork/neuronNetwork.cpp -o neuronNetwork.o
+clang++ -std=c++11 -o testBuild main.cpp console.o neuron.o neuronNetwork.o
 rm *.o
